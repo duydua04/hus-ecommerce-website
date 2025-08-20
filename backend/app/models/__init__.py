c8e467be73a204aa714ce1054c11de1ca33828be
Hoang Dinh Duy <duycp102k3@gmail.com>
2025-08-20 16:26:17 +0700

    Create and complete init models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,16 @@
+from address import Address, SellerAddress, BuyerAddress
+from cart import ShoppingCart, ShoppingCartItem
+from catalog import Category, Carrier, Product, ProductVariant, ProductSize, ProductImage, Discount
+from order import Order, OrderItem
+from review import Review, ReviewImage, ReviewReply
+from users import Buyer, Seller, Admin
+
+__all__ = [
+    "Address", "SellerAddress", "BuyerAddress",
+    "ShoppingCart", "ShoppingCartItem",
+    "Category", "Carrier", "Product", "ProductVariant",
+    "ProductSize", "ProductImage", "Discount",
+    "Order", "OrderItem",
+    "Review", "ReviewImage", "ReviewReply",
+    "Buyer", "Seller", "Admin"
+]
