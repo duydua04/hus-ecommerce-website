34f6fbf5a104cdde76c0aa0a88480ce96fe97736
Hoang Dinh Duy <duycp102k3@gmail.com>
2025-08-24 10:19:59 +0700

    Edit init

--- backend/app/config/__init__.py
+++ backend/app/config/__init__.py
@@ -1,6 +1,6 @@
 from .settings import settings
 from .db import Base, engine, SessionLocal, get_db
-from .s3 import presign_get, presign_put, public_url
+from .s3 import presign_get, presign_put, public_url, create_s3_client, get_s3_client
 
 __all__ = [
     "settings",
@@ -8,7 +8,8 @@ __all__ = [
     "engine",
     "SessionLocal",
     "get_db",
-    "s3",
+    "create_s3_client",
+    "get_s3_client",
     "presign_put",
     "presign_get",
     "public_url",
