75e214f3323c90619ecbdc83a75a37ff74a57b5b
Hoang Dinh Duy <duycp102k3@gmail.com>
2025-08-24 10:07:04 +0700

    Add storage_service file

--- backend/app/config/__init__.py
+++ backend/app/config/__init__.py
@@ -1,6 +1,6 @@
 from .settings import settings
 from .db import Base, engine, SessionLocal, get_db
-from .s3 import s3, presign_get, presign_put, public_url
+from .s3 import presign_get, presign_put, public_url
 
 __all__ = [
     "settings",
