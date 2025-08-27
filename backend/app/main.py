38eb9ad4f40c9f52709bbd76bfe6566d36d0c406
Hoang Dinh Duy <duycp102k3@gmail.com>
2025-08-27 15:40:39 +0700

    Add seller address router

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from .controller.auth_controller import router as auth_router
 from .controller.avatar_controller import router as avatar_router
 from .controller.buyer_address_controller import router as buyer_address_router
+from .controller.seller_address_controller import router as seller_address_router
 app = FastAPI(title="Ecommerce Website")
 app.include_router(auth_router)
 app.include_router(avatar_router)
 app.include_router(buyer_address_router)
+app.include_router(seller_address_router)
