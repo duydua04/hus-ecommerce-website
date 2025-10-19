53164eadfd460fb4c7f03fbfb07713aaf1041c06
Hoang Dinh Duy <duycp102k3@gmail.com>
2025-10-19 20:17:17 +0700

    Login with google

---
+++ backend/app/utils/google_auth.py
@@ -0,0 +1,12 @@
+# app/utils/google_oauth.py
+from authlib.integrations.starlette_client import OAuth
+from ..config.settings import settings
+
+oauth = OAuth()
+google = oauth.register(
+    name="google",
+    client_id=settings.GOOGLE_CLIENT_ID,
+    client_secret=settings.GOOGLE_CLIENT_SECRET,
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    client_kwargs={"scope": "openid email profile"},
+)
