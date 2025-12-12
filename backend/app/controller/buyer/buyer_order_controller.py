e3557b151bb01d84e290b90d5cb83e72c1b30c6d
Nguyen Hoa <23001521@hus.edu.vn>
2025-12-12 10:41:32 +0700

    Add checkout total

--- backend/app/controller/buyer/buyer_order_controller.py
+++ backend/app/controller/buyer/buyer_order_controller.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session, Query as SAQuery
 from ...config.db import get_db
 from ...services.buyer import buyer_order_service
 from ...models.catalog import Product, ProductImage, ProductSize, ProductVariant
-from ...services.buyer.buyer_order_service import ApplyDiscountRequest
+from ...services.buyer.buyer_order_service import ApplyDiscountRequest, CheckoutTotalRequest
 from pydantic import BaseModel
 from decimal import Decimal
 from sqlalchemy.orm import selectinload
@@ -35,3 +35,26 @@ def select_carrier(
     Khi user chọn DVVC, trả về phí ship và tổng thanh toán
     """
     return buyer_order_service.calculate_total_with_shipping(db, carrier_id, total_weight, subtotal, discount_amount)
+
+# ============= GỘP CÁC BƯỚC TRONG THANH TOÁN ====================
+@router.post("/total")
+def checkout_total(
+    payload: CheckoutTotalRequest,
+    db: Session = Depends(get_db),
+    buyer_id: int = 1
+):
+    """
+    FE gửi payload JSON:
+    {
+        "selected_cart_item_ids": [10, 11, 13],
+        "discount_code": "ABC123",
+        "carrier_id": 1
+    }
+    """
+    return buyer_order_service.checkout_total(
+        db=db,
+        buyer_id=buyer_id,
+        selected_cart_item_ids=payload.selected_cart_item_ids,
+        discount_code=payload.discount_code,
+        carrier_id=payload.carrier_id
+    )
