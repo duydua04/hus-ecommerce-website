49447530d7948ffb59dc8dc3bdf8731fb37f2087
Hoang Dinh Duy <duycp102k3@gmail.com>
2026-01-04 20:52:57 +0700

    Application packaging successful

--- app-buyer/page/order_payment/order-payment.js
+++
@@ -1,8 +0,0 @@
-// Chọn cách thanh toán
-const paymentOptions = document.querySelectorAll('.payment-option');
-paymentOptions.forEach(option => {
-    option.addEventListener('click', function() {
-        paymentOptions.forEach(opt => opt.classList.remove('active'));
-        this.classList.add('active');
-    });
-});
